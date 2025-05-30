7f43f06fb0cff568b3190f2325cf9701e6bd30af
ifindv <ifindv@gmail.com>
2025-05-30 10:17:01 +0800

    导航栏增加毛玻璃效果

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,5 +1,4 @@
 // add copy button for code block
-
 document.addEventListener('DOMContentLoaded', (event) => {
   const codeBlocks = document.querySelectorAll('pre');
   codeBlocks.forEach((block) => {
@@ -24,4 +23,23 @@ document.addEventListener('DOMContentLoaded', (event) => {
     });
     block.appendChild(button);
   });
+});
+
+// add glass effect to navbar when scrolled
+document.addEventListener('DOMContentLoaded', (event) => {
+  const navbar = document.querySelector('.navbar');
+  let isScrolled = false;
+
+  const handleScroll = () => {
+    const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    if (scrollTop > 0 && !isScrolled) {
+      navbar.classList.add('glass');
+      isScrolled = true;
+    } else if (scrollTop === 0 && isScrolled) {
+      navbar.classList.remove('glass');
+      isScrolled = false;
+    }
+  };
+
+  window.addEventListener('scroll', handleScroll);
 });
